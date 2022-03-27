2ddd27bd961e428ab6543894f4e61e228d0572f3
TomerGrad <101577782+TomerGrad@users.noreply.github.com>
2022-03-27 21:31:55 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Main Connect 4 game file.
+Run and play!
+(c) Tomer Grad plain games package.
+Play with fun and care!
+"""
+from gui import Connect4Board
+if __name__ == '__main__':
+    game = Connect4Board()
+    game.mainloop()
